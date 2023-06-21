2c5192b5cc061ccce3637effdc1510b795b5e75c
Сергей Бразгун <sergey.brazgun@yandex.ru>
2023-06-21 12:15:13 +0300

    четвертая отправка на ревью

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,74 +1,11 @@
 from django.contrib import admin
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
 
-from .models import Category, Genre, GenreTitle, Title, User, Review, Comments
+from .models import Category, GenreTitle, Title, User, Review, Comments
 
 
-class TitleResource(resources.ModelResource):
-
-    class Meta:
-        model = Title
-
-
-class TitleAdmin(ImportExportModelAdmin):
-    resource_classes = [TitleResource]
-
-
-class UserResource(resources.ModelResource):
-
-    class Meta:
-        model = User
-
-
-class UserAdmin(ImportExportModelAdmin):
-    resource_classes = [UserResource]
-
-
-class CategoryResource(resources.ModelResource):
-
-    class Meta:
-        model = Category
-
-
-class CategoryAdmin(ImportExportModelAdmin):
-    resource_classes = [CategoryResource]
-
-
-class GenreResource(resources.ModelResource):
-
-    class Meta:
-        model = Genre
-
-
-class GenreAdmin(ImportExportModelAdmin):
-    resource_classes = [GenreResource]
-
-
-class GenreResource(resources.ModelResource):
-
-    class Meta:
-        model = Genre
-
-
-class GenreAdmin(ImportExportModelAdmin):
-    resource_classes = [GenreResource]
-
-
-class GenreTitleResource(resources.ModelResource):
-
-    class Meta:
-        model = GenreTitle
-
-
-class GenreTitleAdmin(ImportExportModelAdmin):
-    resource_classes = [GenreTitleResource]
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Genre, GenreAdmin)
-admin.site.register(Title, TitleAdmin)
-admin.site.register(GenreTitle, GenreTitleAdmin)
-admin.site.register(User, UserAdmin)
+admin.site.register(Category, )
+admin.site.register(Title, )
+admin.site.register(GenreTitle, )
+admin.site.register(User, )
 admin.site.register(Comments,)
 admin.site.register(Review, )
